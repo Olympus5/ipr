5ada38e4e07d4754c0c9e8827be7fe6894315ced
Erwan IQUEL <erwan.iquel@gmail.com>
2018-02-04 12:25:43 +0100

    RMI Echo - registre dans bin ok

--- tp2/src/tp/rmi/client/ChatClient.java
+++ tp2/src/tp/rmi/client/ChatClient.java
@@ -3,9 +3,12 @@
  */
 package tp.rmi.client;
 
+import java.net.InetAddress;
 import java.net.MalformedURLException;
+import java.net.UnknownHostException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 import tp.rmi.common.ChatRemote;
@@ -20,11 +23,31 @@ public class ChatClient {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+
 		try {
-			ChatRemote cr = (ChatRemote) Naming.lookup("//localhost/chat");
-		} catch (MalformedURLException | RemoteException | NotBoundException e) {
+			//Mise en place du Security Manager coté client
+			/*if(System.getSecurityManager() == null) {
+				System.setSecurityManager(new SecurityManager());
+			}*/
+
+			//On charge la méthodé distribué par notre serveur RMI
+			Remote r = Naming.lookup("rmi://" + InetAddress.getLocalHost().getHostName() + "/ChatRemote");
+			
+			//On check si c'est bien le bon objet qui nous a été renvoyé
+			if(r instanceof ChatRemote) {
+				System.out.println(((ChatRemote) r).echo("moi", "test"));
+			} else {
+				System.err.println("Mauvaise classe chargé: " + (ChatRemote.class));
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (NotBoundException e) {
 			e.printStackTrace();
 		}
 	}
-
 }
--- tp2/src/tp/rmi/server/ChatServer.java
+++ tp2/src/tp/rmi/server/ChatServer.java
@@ -3,12 +3,12 @@
  */
 package tp.rmi.server;
 
+import java.net.InetAddress;
 import java.net.MalformedURLException;
-import java.nio.file.Paths;
+import java.net.UnknownHostException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
-
-import tp.rmi.common.ChatRemote;
+import java.rmi.registry.LocateRegistry;
 
 /**
  * @author Erwan IQUEL, Mathieu LE CLEC'H
@@ -17,12 +17,36 @@ import tp.rmi.common.ChatRemote;
 public class ChatServer {
 	public static void main(String[] args) {
 		try {
-			String path = Paths.get("bin").toUri().toURL().toString();
-			System.setProperty("java.rmi.server.codebase", path);
+			/*
+			 * Il est possible de créer un registre dynamiquement, cela est avantageux quand nous avons un Serveur
+			 * Qui instanci l'ensemble des méthodes qui vont être distribués:
+			 
+			 LocateRegistry.createRegistry(1099);
+			 
+			 * Plus besoin de lancer dans bin ou de spécifier le codebase pour le registry :)
+			 */
 			
-			ChatRemote cr = new ChatRemoteImpl();
-			Naming.rebind("//localhost/echo", cr);
-		} catch (RemoteException | MalformedURLException e) {
+			// On met en place notre Security Manager
+			/*System.out.println("Mise en place du Security Manager...");
+			if(System.getSecurityManager() == null) {
+				System.setSecurityManager(new SecurityManager());
+			}*/
+			
+			// On instancie l'objet qui va être distribué par le serveur
+			ChatRemoteImpl chatRemoteImpl = new ChatRemoteImpl();
+			
+			//On enregsitre notre objet dans le registre RMI
+			String url = "rmi://" + InetAddress.getLocalHost().getHostName() + "/ChatRemote";
+			System.out.println("Enregistrement de l'objet avec l'url: " + url + "...");	
+			Naming.rebind(url, chatRemoteImpl);
+			
+			System.out.println("Serveur en marche.");
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
