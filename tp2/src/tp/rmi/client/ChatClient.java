916c4d119a0becdd8c564af149281386fb67c7e9
Erwan IQUEL <erwan.iquel@gmail.com>
2018-02-04 15:04:05 +0100

    RMI echo - fin

--- tp2/src/tp/rmi/client/ChatClient.java
+++ tp2/src/tp/rmi/client/ChatClient.java
@@ -1,5 +1,8 @@
 package tp.rmi.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.UnknownHostException;
@@ -21,6 +24,11 @@ public class ChatClient {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		if(args.length < 1) {
+			System.err.println("Veuillez rentrer un pseudo.");
+			System.exit(-1);
+		}
+		
 		try {
 			String path = Paths.get("bin").toUri().toURL().toString();
 			System.setProperty("java.rmi.codebase", path);
@@ -35,9 +43,15 @@ public class ChatClient {
 			
 			//On check si c'est bien le bon objet qui nous a été renvoyé
 			if(r instanceof ChatRemote) {
+				String message = "";
+				
+				while(!message.equals("FIN")) {
+					message = lireMessageAuClavier();
+					System.out.println(((ChatRemote) r).echo(args[0], message));
+				}
 				System.out.println(((ChatRemote) r).echo("moi", "test"));
 			} else {
-				System.err.println("Mauvaise classe chargé: " + (ChatRemote.class));
+				System.err.println("Mauvaise classe chargé: " + (r.getClass()));
 			}
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
@@ -47,6 +61,20 @@ public class ChatClient {
 			e.printStackTrace();
 		} catch (NotBoundException e) {
 			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Lit un message venant du clavier
+	 * @return Message lu au clavier
+	 * @throws IOException
+	 */
+	public static String lireMessageAuClavier() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Votre message: ");
+		return br.readLine();
 	}
 }
