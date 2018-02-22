64975d0e60cb570710e55e80d197c9abb0168821
Erwan IQUEL <erwan.iquel@gmail.com>
2018-02-22 15:41:40 +0100

    TP MoM - Routage topic ok

---
+++ tp3/src/fr/istic/date/topic/RecevoirTouteDate.java
@@ -0,0 +1,54 @@
+package fr.istic.date.topic;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeoutException;
+
+public class RecevoirTouteDate {
+  private static final String EXCHANGE_NAME = "date_topic";
+  private static final String KEY = "date.#";
+
+  public static void main(String[] argv) {
+    ConnectionFactory factory = new ConnectionFactory();
+    Connection connection = null;
+    Channel channel = null;
+    
+    try {
+		factory.setUri("amqp://lyfxesxf:aPjKUUBJuBdBcgqCeDg9z5O5Ptkt117Q@sheep.rmq.cloudamqp.com/lyfxesxf");
+		
+	    connection = factory.newConnection();
+	    channel = connection.createChannel();
+
+	    channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+	    String queueName = channel.queueDeclare().getQueue();
+	    channel.queueBind(queueName, EXCHANGE_NAME, KEY);
+	    
+	    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+	    Consumer consumer = new DefaultConsumer(channel) {
+	      @Override
+	      public void handleDelivery(String consumerTag, Envelope envelope,
+	                                 AMQP.BasicProperties properties, byte[] body) throws IOException {
+	        String message = new String(body, "UTF-8");
+	        System.out.println(" [x] Received '" + message + "'");
+	      }
+	    };
+	    channel.basicConsume(queueName, true, consumer);
+	} catch (KeyManagementException e) {
+		e.printStackTrace();
+	} catch (NoSuchAlgorithmException e) {
+		e.printStackTrace();
+	} catch (URISyntaxException e) {
+		e.printStackTrace();
+	} catch (IOException e) {
+		e.printStackTrace();
+	} catch (TimeoutException e) {
+		e.printStackTrace();
+	}
+  }
+}
+
