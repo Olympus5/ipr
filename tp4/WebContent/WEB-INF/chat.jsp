7c48502457a0da8552103510a7acd009eb9242f8
Erwan IQUEL <erwan.iquel@gmail.com>
2018-04-05 11:40:28 +0200

    Pour que tu termines le rapport d'IPR (j'avais oublier de commit le tp4)

--- tp4/WebContent/WEB-INF/chat.jsp
+++ tp4/WebContent/WEB-INF/chat.jsp
@@ -1,17 +1,11 @@
-
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Insert title here</title>
+		<title>TP IPR - Java EE</title>
 	</head>
 	<body>
 		<h1>Mon super Chat</h1>
-		
-		<p>
-			Bienvenue sur le chat <br />
-			Soyez polis
-		</p>
 		
 		<form method="post" action="Chat">
 			<label for="message">Votre message: </label>
@@ -21,7 +15,7 @@
 		</form>
 		
 		<p id="chat">
-			${ content }
+			<c:out value="${ content }" escapeXml="false"/>
 		</p>
 	</body>
 </html>
--- tp4/src/fr/istic/ipr/servlet/Chat.java
+++ tp4/src/fr/istic/ipr/servlet/Chat.java
@@ -3,11 +3,14 @@ package fr.istic.ipr.servlet;
 import java.io.IOException;
 import java.time.LocalDateTime;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/Chat")
 public class Chat extends HttpServlet {
@@ -18,21 +21,52 @@ public class Chat extends HttpServlet {
         super();
     }
 
+    public void init(ServletConfig config) throws ServletException {
+    	super.init(config);
+    	buffer.append(config.getServletContext().getInitParameter("welcome") + "<br />");
+    }
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		//Si il existe pas de variable nom alors redirection a la page static index.html
+		if(session.getAttribute("nom") == null) {
+			this.getServletContext().getRequestDispatcher("/index.html").forward(request, response);
+		}
+		
 		request.setAttribute("content", buffer);
 		
 		this.getServletContext().getRequestDispatcher("/WEB-INF/chat.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String action = request.getParameter("action");
-		String message = request.getParameter("message");
+		HttpSession session = request.getSession();
+		String nom = (String) session.getAttribute("nom");
 		
-		if("submit".equals(action)) {
-			buffer.append("(" + LocalDateTime.now() + ") " + message + "<br />");
+		if(nom == null) {
+			nom = request.getParameter("nom");
+			
+			if(nom == null) { // Si nom est pas présent => redirection index.html
+				this.getServletContext().getRequestDispatcher("/index.html").forward(request, response);
+			} else {
+				if(nom.length() <= 0) {//Si nom est vide => redirection index.html
+					this.getServletContext().getRequestDispatcher("/index.html").forward(request, response);
+				} else {// Ajout du nom dans la session utilisateur
+					session.setAttribute("nom", nom);
+					this.getServletContext().getRequestDispatcher("/WEB-INF/chat.jsp").forward(request, response);
+				}
+			}
+		} else{// User loggé, donc autorisé à envoyer des messages
+			String action = request.getParameter("action");
+			String message = request.getParameter("message");
+			
+			if("submit".equals(action)) {
+				buffer.append("(" + LocalDateTime.now() + ")"+ nom +">" + message + "<br />");
+			}
+			
+			doGet(request, response);
 		}
 		
-		doGet(request, response);
 	}
 
 }
